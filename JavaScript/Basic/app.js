a8f8547c04e7228cac7fb39c321e7fec99e41bef
May Jarung <89119756+MayJarung@users.noreply.github.com>
2023-10-13 05:10:03 +0700

    Add files via upload

---
+++ JavaScript/Basic/app.js
@@ -0,0 +1,12 @@
+// แสดงข้อความบนเว็บ
+document.write("เมย์สวยมาก2")
+document.write("<p>May is so cute</p>")
+
+// pop up แจ้งเตือนก่อนแสดงเนื้อหาบนเว็บ
+alert("ถ้าคุณสวย กด 'ตกลง'")
+
+// รูปแบบของการแสดงข้อความผ่านทางแถบ console ใน website
+console.log("Hello Im here")
+console.error("พบไวรัสความสวยของเมย์")
+console.warn("พบข้อผิดพลาดเนื่องจากความน่ารักของเมย์")
+
---
+++ JavaScript/Basic/item1.js
@@ -0,0 +1,16 @@
+let namee = "May Jarung", age = 20 
+let num1 = "20"+10 // เอาข้อความต่อกับตัวเลข จะได้ 2010
+let num2 = 20+10   // เอาตัวเลขบวกกัน จะได้ 30
+let num3 = true+10 // เอา 1 + 10 = 11
+
+console.log("Name : ",namee)
+document.write(namee)
+console.log("Age : ",age)
+
+let x = parseInt("20.15")
+let y = parseFloat("20.15")
+let z = +"20.15"
+
+console.log(typeof(x))
+console.log(typeof(y))
+console.log(typeof(z))
